b21128aa9a0b47a34ac11503be36ce49e43ed91d
Abode Chikh <Abdulahad.chikh@nordicstation.com>
2020-05-14 21:54:10 +0200

    Fixade gitignore och startade test projekt

---
+++ frontend-test/cypress/integration/test-suite.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+
+//Test suite
+describe("Resgression test suite", function(){
+
+    //Test case
+    it("Test case 1", function(){
+        cy.visit("http://localhost:3000")
+
+    })
+
+})
